b87732e20b1e25530070863b8f50de1a754b8593
Korostelyova Anna <annakoro2001@gmail.com>
2021-01-02 16:27:07 +0300

    Old files deleted, new added to lesson-3

--- 2-4.py
+++
@@ -1,8 +0,0 @@
-userstr = input('Введите строку (слова, разделенные пробелами): ')
-userwords = userstr.split()
-newstr = []
-for userword in userwords:
-    newword = userword[:10]
-    newstr.append(newword)
-for ind, newword in enumerate(newstr):
-    print(ind, newword)
